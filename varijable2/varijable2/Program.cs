2788f287fbd525ce52afd06e9ad945b948d53325
DavidCuturic5 <david.cuturic5@gmail.com>
2024-10-10 15:20:57 +0200

    Add files via upload

---
+++ varijable2/varijable2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace varijable2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesite jedan cijeli broj i jedan decimalni broj, odvojeni ramakom");
+
+            string unos = Console.ReadLine();
+
+            string[] razmak = unos.Split(' ');
+
+            int a = int.Parse(razmak[0]);
+            double b = double.Parse(razmak[1]);
+
+            int x = a * 2;
+            double y = b * 2;
+
+            Console.WriteLine($"Dvostruka vrijednost prvog(cijelog) broja je: {x}");
+            Console.WriteLine($"Dvostruka vrijednost drugog(decimalnog) broja je: {y}");
+
+            Console.ReadKey();
+        }
+    }
+}
